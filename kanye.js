df6777bda46ac23ee409cf14412ba296d095329a
Nima Boscarino <nima.boscarino@gmail.com>
2018-06-19 00:26:18 -0700

    complete! (only ned to check for empty/clear)

--- kanye.js
+++ kanye.js
@@ -3,6 +3,7 @@ let sounds = [
     'dadada.wav',
     'heh.wav',
     'scream.wav',
+    // strong.wav
 ]
 
 let kanyes = document.querySelectorAll('.sound-list img')
@@ -10,6 +11,25 @@ kanyes.forEach((kanye, index) => {
     var audio = new Audio(sounds[index]);
 
     kanye.addEventListener('click', (e) => {
+        audio.play();
+    })
+})
+
+let newSoundButton = document.querySelector('#new-sound')
+newSoundButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    let imagePath = document.querySelector('input[name=kanye-image]')
+    let soundPath = document.querySelector('input[name=kanye-sound]')
+
+    newImage = document.createElement('img')
+    newImage.src = imagePath.value
+
+    let kanyeContainer = document.querySelector('.sound-list')
+    kanyeContainer.appendChild(newImage)
+
+    var audio = new Audio(soundPath.value);
+    newImage.addEventListener('click', (e) => {
         audio.play();
     })
 })
